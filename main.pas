e3449007dc5ea5177370ebeddef8fd5b4d37450b
Porridge01 <ivanov.u97@gmail.com>
2015-11-05 02:29:43 +1000

    Add ColorPalette

--- main.pas
+++ main.pas
@@ -6,15 +6,21 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, ExtCtrls,
-  ComCtrls, Buttons, Menus, ExtDlgs, ColorBox, StdCtrls, UTools, UFigures,
-  UAbout;
+  ComCtrls, Buttons, Menus, ExtDlgs, ColorBox, StdCtrls, Grids, UTools,
+  UFigures, UAbout;
 
 type
 
   { TMainForm }
 
   TMainForm = class(TForm)
-    SetColorBox: TColorBox;
+    FillColorDialog: TColorDialog;
+    PenColorDialog: TColorDialog;
+    DrawGrid: TDrawGrid;
+    PalettePanel: TPanel;
+    Panel1: TPanel;
+    PColorPanel: TPanel;
+    FillColorPanel: TPanel;
     SetWidthBox: TComboBox;
     Save: TMenuItem;
     MMenu: TMainMenu;
@@ -29,6 +35,11 @@ type
     ScrollBox: TScrollBox;
     Stat1: TStatusBar;
     procedure AboutClick(Sender: TObject);
+    procedure DrawGridDrawCell(Sender: TObject; aCol, aRow: Integer;
+      aRect: TRect; aState: TGridDrawState);
+    procedure DrawGridMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure FillColorPanelClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure PaintBoxMouseDown(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer);
@@ -37,8 +48,9 @@ type
     procedure PaintBoxMouseUp(Sender: TObject; Button: TMouseButton;
       Shift: TShiftState; X, Y: Integer);
     procedure PaintBoxPaint(Sender: TObject);
+    procedure PColorPanelClick(Sender: TObject);
     procedure SaveClick(Sender: TObject);
-    procedure SetColorBoxChange(Sender: TObject);
+    procedure SetColor(Shift: TShiftState);
     procedure SetWidthBoxChange(Sender: TObject);
     procedure ToolClick(Sender: TObject);
     procedure YExitClick(Sender: TObject);
@@ -52,6 +64,11 @@ var
   MainForm: TMainForm;
   IndexOfBtn: integer;
   SetBtn: TBitBtn;
+
+const
+  ArColors: array[0..15] of TColor = (clWhite, clBlack, clMaroon, clGreen,
+    clOlive, clNavy, clPurple, clTeal, clRed, clLime, clYellow, clBlue,
+    clFuchsia, clAqua, clGray, clSilver);
 
 implementation
 
@@ -86,6 +103,7 @@ procedure TMainForm.PaintBoxMouseDown(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 begin
   if (ssLeft in Shift) and (ssRight in Shift) then Exit;
+  SetColor(Shift);
   TTool.Tools[IndexOfBtn].OnMouseDown(Shift, X,Y);
 end;
 
@@ -111,11 +129,54 @@ begin
   AboutForm.ShowModal;
 end;
 
+procedure TMainForm.DrawGridDrawCell(Sender: TObject; aCol, aRow: Integer;
+  aRect: TRect; aState: TGridDrawState);
+begin
+  with DrawGrid.Canvas do begin
+    Brush.Color:= ArColors[aCol * 2 + aRow];
+    FillRect(aRect);
+  end;
+end;
+
+procedure TMainForm.DrawGridMouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+var
+  Col, Row: integer;
+begin
+  DrawGrid.MouseToCell(X, Y, Col, Row);
+  if Shift = [ssLeft] then
+    begin
+      PaintBox.Canvas.Pen.Color := ArColors[Col * 2 + Row];
+      PColorPanel.Color := ArColors[Col * 2 + Row];
+      PenColorDialog.Color := ArColors[Col * 2 + Row];
+    end;
+    if (Shift = [ssRight]) then
+    begin
+      PaintBox.Canvas.Brush.Color := ArColors[Col * 2 + Row];
+      FillColorPanel.Color := ArColors[Col * 2 + Row];
+      FillColorDialog.Color := ArColors[Col * 2 + Row];
+    end;
+end;
+
+procedure TMainForm.FillColorPanelClick(Sender: TObject);
+begin
+  FillColorDialog.Execute;
+  PaintBox.Canvas.Brush.Color := FillColorDialog.Color;
+  FillColorPanel.Color := FillColorDialog.Color;
+end;
+
 procedure TMainForm.PaintBoxPaint(Sender: TObject);
 var figure: TFigure;
 begin
   for figure in TFigure.YFigures do
       figure.Draw(PaintBox.Canvas);
+end;
+
+procedure TMainForm.PColorPanelClick(Sender: TObject);
+begin
+  PenColorDialog.Execute;
+  PaintBox.Canvas.Pen.Color := PenColorDialog.Color;
+  PColorPanel.Color := PenColorDialog.Color;
 end;
 
 procedure TMainForm.SaveClick(Sender: TObject);
@@ -140,9 +201,12 @@ begin
   end;
 end;
 
-procedure TMainForm.SetColorBoxChange(Sender: TObject);
+procedure TMainForm.SetColor(Shift: TShiftState);
 begin
-  TTool.SetColor(SetColorBox.Selected);
+  if Shift = [ssLeft] then
+  TTool.SetColor(PColorPanel.Color);
+  if Shift = [ssRight] then
+  TTool.SetColor(FillColorPanel.Color);
 end;
 
 procedure TMainForm.SetWidthBoxChange(Sender: TObject);
